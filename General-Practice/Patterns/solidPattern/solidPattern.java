3c4ed149c5fefd621a60d7feba7e770d903f3d0c
Piyush Kumar <piiyushkumar234@gmail.com>
2023-06-13 15:29:43 +0530

    added solidPattern

---
+++ General-Practice/Patterns/solidPattern/solidPattern.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class solidPattern {
+
+    public static void main(String[] args) {
+        System.out.println("Enter size of solid pattern: ");
+        Scanner sc = new Scanner (System.in);
+        int n = sc.nextInt();
+
+        for(int i = 0; i<n;i++){
+            for(int j = 0; j<n; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+    }
+}

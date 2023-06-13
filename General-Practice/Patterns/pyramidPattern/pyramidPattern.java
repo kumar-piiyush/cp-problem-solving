dd4091aae8b691f58ef877c6588d7dac329f4e48
Piyush Kumar <piiyushkumar234@gmail.com>
2023-06-13 16:01:12 +0530

    borderPattern

---
+++ General-Practice/Patterns/pyramidPattern/pyramidPattern.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class pyramidPattern {
+    public static void main(String[] args) {
+        System.out.println("Enter n: ");
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i==j) {
+                    System.out.print("*");
+                }else {
+                    System.out.print(" ");
+                }
+                
+            }
+            System.out.println();
+        }
+    }
+}

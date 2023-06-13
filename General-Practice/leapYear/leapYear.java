d31a8912123ed08b29b0160da68d22e260e8951f
Piyush Kumar <piiyushkumar234@gmail.com>
2023-06-13 14:56:43 +0530

    code complete

--- General-Practice/leapYear/leapYear.java
+++ General-Practice/leapYear/leapYear.java
@@ -7,6 +7,9 @@ public class leapYear {
 
     public static boolean isLeapYear(int year){
 
+        if ((year%4)==0) return true;
+        else if((year%400)==0) return true;
+        else if((year%100)==0) return false;
         return false;
     }
 

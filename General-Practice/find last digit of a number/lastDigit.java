768f5fdc194e8a7ded48c3cb781b4d17de02dde0
Piyush Kumar <piiyushkumar234@gmail.com>
2023-06-13 12:14:57 +0530

    code complete

--- General-Practice/find last digit of a number/lastDigit.java
+++ General-Practice/find last digit of a number/lastDigit.java
@@ -1,5 +1,18 @@
+import java.util.*;
+
 public class lastDigit{
+
+    public static int findLastDigit(int n){        
+        return n%10;
+    }
     public static void main(String args[]){
         System.out.println("Enter the number: ");
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        int ans = findLastDigit(n);
+        System.out.println("Last digit of the entered number is: "+ans);
+
     }
 }
